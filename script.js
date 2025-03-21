ba583b54c860b83f81392616475c816970da9458
Renan Fumis <renan-oliveira19@hotmail.com>
2025-03-20 22:21:18 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const images = [
+  'image/blurred-imagem-drive-wingpg22.jpg',
+  'image/blurred-image-simtronic-pg62.png',
+  'image/blurred-image-wing-pg31.png'
+];
+
+let currentImageIndex = 0;
+
+function changeBackground() {
+  const backgroundImages = document.querySelectorAll('.background img');
+  backgroundImages.forEach(img => img.classList.remove('active'));
+
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  backgroundImages[currentImageIndex].classList.add('active');
+}
+
+// Inicializa o carrossel
+function initCarousel() {
+  const backgroundDiv = document.querySelector('.background');
+
+  images.forEach((src, index) => {
+    const img = document.createElement('img');
+    img.src = src;
+    if (index === 0) img.classList.add('active');  // Inicia com a primeira imagem visível
+    backgroundDiv.appendChild(img);
+  });
+
+  setInterval(changeBackground, 10000);  // Troca a imagem a cada 10 segundos
+}
+
+document.addEventListener('DOMContentLoaded', initCarousel);
